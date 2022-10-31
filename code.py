d1ffb9943ef05174477bcfb4c33cd7e6ad49b070
lucas-debruyn <113041278+lucas-debruyn@users.noreply.github.com>
2022-10-31 10:58:37 -0400

    Update code.py

--- code.py
+++ code.py
@@ -14,14 +14,14 @@ def game_scene():
     # image banks for CircuitPython
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
     image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
-   
+
     # set background to image 0 in the image bank
     #   and the size (10x8 tiles of size 16x16)
     background = stage.Grid(image_bank_background, 10, 8)
 
     # a sprite that will be updated every frame
     ship = stage.Sprite(ugame.display, 5, 75, 66)
-    
+
     # create a stage for the background to show up on
     #   and set the frame rate to 60fps
     game = stage.Stage(ugame.display, 60)
