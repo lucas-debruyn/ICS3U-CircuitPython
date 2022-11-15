8b7053293034b80d77d2a0c7558ba3c5f376a0e0
lucas-debruyn <113041278+lucas-debruyn@users.noreply.github.com>
2022-11-15 17:18:05 -0500

    Update code.py

--- code.py
+++ code.py
@@ -138,8 +138,9 @@ def game_scene():
     sound.mute(False)
 
     # set the background to image 0 in the image bank
-    background = stage.Grid(image_bank_background, constants.SCREEN_GRID_X,
-        constants.SCREEN_GRID_Y)
+    background = stage.Grid(
+        image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y
+    )
     for x_location in range(constants.SCREEN_GRID_X):
         for y_location in range(constants.SCREEN_GRID_Y):
             tile_picked = random.randint(1, 3)
